b51acacae856db4390e600731895ba298eb9e3ed
chernrom <chernikovdsign@gmail.com>
2025-07-04 13:54:33 +0300

    v1.1: добавлен цикл, ввод с проверкой, улучшена обработка ошибок

--- main.go
+++ main.go
@@ -2,34 +2,79 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"time"
 )
 
 func main() {
-	var a int
-	var b int
-	var op string
-	var result float64
 
-	fmt.Println("Введите первое число:")
-	fmt.Scanln(&a)
-	fmt.Println("выберите знак")
-	fmt.Scanln(&op)
-	if op != "+" && op != "-" && op != "*" && op != "/" {
-		fmt.Println("Неверный знак. Выберите из +, -, *, /")
+	for {
+
+		var a int
+		var b int
+		var op string
+		var result float64
+		var input string
+
+		fmt.Println("Это калькулятор")
+		fmt.Println("Пока он может выполнять только простейшие функции")
+		time.Sleep(1000 * time.Millisecond)
+		fmt.Println("Если захотите выйти - введите exit вместо числа")
+		fmt.Println("Можем начинать!")
+		fmt.Println("-----------------")
+		time.Sleep(1000 * time.Millisecond)
+		fmt.Println("Введите первое число:")
+		fmt.Scanln(&input)
+
+		if input == "exit" {
+			break
+		}
+
+		a, err := strconv.Atoi(input)
+		if err != nil {
+			fmt.Println("Введите целое число!")
+			break
+		}
+
+		for {
+			fmt.Println("выберите действие(+, -, * или /)")
+			fmt.Scanln(&op)
+
+			if op != "+" && op != "-" && op != "*" && op != "/" {
+				fmt.Println("Неверный знак. Выберите из +, -, *, /")
+				continue
+			}
+			break
+		}
+
+		fmt.Println("Введите второе число:")
+		fmt.Scanln(&input)
+
+		if input == "exit" {
+			continue
+		}
+
+		b, err = strconv.Atoi(input)
+		if err != nil {
+			fmt.Println("Введите целое число!")
+			continue
+		}
+
+		switch op {
+		case "+":
+			result = float64(a + b)
+		case "-":
+			result = float64(a - b)
+		case "*":
+			result = float64(a * b)
+		case "/":
+			if b == 0 {
+				fmt.Println("На 0 делить нельзя")
+				continue
+			}
+			result = float64(a) / float64(b)
+		}
+		fmt.Printf("Ответ: %.2f\n", result)
+		fmt.Println("-----------------")
 	}
-	fmt.Println("Введите второе число:")
-	fmt.Scanln(&b)
-
-	switch op {
-	case "+":
-		result = float64(a + b)
-	case "-":
-		result = float64(a - b)
-	case "*":
-		result = float64(a * b)
-	case "/":
-		result = float64(a / b)
-	}
-
-	fmt.Printf("Ответ: %.2f\n", result)
 }
